7965051eaea34c8e45645cbe537fc621d3fa7af3
Theo Julienne <theo@icy.com.au>
2014-04-27 15:19:02 +1000

    Add fix for invalid temperatures coming from cloud.

--- pyninja/devices/TemperatureNinjaDevice.py
+++ pyninja/devices/TemperatureNinjaDevice.py
@@ -4,7 +4,14 @@ from ..NinjaDevice import NinjaDevice
 class TemperatureNinjaDevice(NinjaDevice):
 	@property
 	def current_temperature( self ):
-		return self._get_data( 'last_data', {} ).get( 'DA', None )
+		data = self._get_data( 'last_data', {} ).get( 'DA', None )
+		if not isinstance(data, float) and not isinstance(data, int):
+			return None
+		return data
 	
 	def get_default_reading( self ):
-		return ('Temperature', '%.1f C' % self.current_temperature)
+		temp = self.current_temperature
+		if temp is None:
+			return ('Temperature', 'Unknown')
+		else:
+			return ('Temperature', '%.1f C' % temp)
